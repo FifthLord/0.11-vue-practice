cabc1f6f3cdf0a09c2be2c56c47e0a78c7c5a42c
FifthLord <nborysenko777@gmail.com>
2023-02-10 11:52:02 +0200

    start work with vue logic

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<!-- template для написання розмітки - це шаблон, структура компоненту -->
+<template>
+   <div>
+      <div>
+         <button v-on:click="addLike">Like</button>
+         <!--@ - більш короткий запис v-on:-->
+         <button @click="addDislike">Dislike</button>
+      </div>
+      <!--{{ }} - спеціальний синтаксис "інтерполяція"-->
+      <div>кількість лайків - {{ likes }}</div>
+      <div>кількість дизлайків - {{ dislikes }}</div>
+   </div>
+</template>
+
+<!-- script для написпання скриптів, функцій, коду компоненту -->
+<script>
+// за зомовчуванням ми повинні єкспортувати об'єкт, всі інше це лише цукор
+export default {
+   //об'єкти з даними
+   data() {
+      return {
+         likes: 0,
+         dislikes: 0,
+      }
+   },
+   //методи для роботи з даними
+   methods: {
+      addLike() {
+         //тут напряму звернули до likes, пропустивши data
+         this.likes += 1;
+      },
+      addDislike() {
+         this.dislikes += 1;
+      },
+   }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 
-
+//створення єкземпляру апки й монтування його по ІД в корневий дів (папка public/index)
 createApp(App).mount('#app')
