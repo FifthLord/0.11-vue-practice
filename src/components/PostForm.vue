d23ea2f9f138a5b62f6ee8d920c70fd9d506630a
FifthLord <nborysenko777@gmail.com>
2023-02-16 20:38:51 +0200

    added v-model logic

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,8 +2,9 @@
    <form class="post-form" @submit.prevent>
          <h4>Створення посту</h4>
 <!--"v-model=" -короткий запис "v-bind:value="+"@input="* = $event.target.value""-->
-         <input v-model="post.title" class="post-input" type="text" placeholder="Назва" />
-         <input v-bind:value="post.body" @input="post.body = $event.target.value" class="post-input" type="text" placeholder="Опис" />
+<!--"v-model:value=" де "value" - це назва нашого пропсу, якщо просто "v-model="", то по замовчуваню буде "modelValue"-->
+         <my-input v-model="post.title" type="text" placeholder="Назва" />
+         <my-input v-model="post.body" type="text" placeholder="Опис" />
          <my-button style="align-self: flex-end; margin-top: 15px;" @click="createPost">Створити</my-button>
       </form>
 </template>
@@ -22,7 +23,7 @@ export default {
       createPost() {
          // створення унікального ІД
          this.post.id = Date.now();
-         // функція $emit - створення події на яку "підпишеться" батьківський компонент (аналог event) 
+         // функція $emit - створення події на яку "підпишеться" батьківський компонент через v-model(аналог event) 
          // перший параметр - це завжди назва "події", 
          // натупні - це аргументи функції які будуть передані батьківському компоненту
          this.$emit('create', this.post);
@@ -39,12 +40,5 @@ export default {
 .post-form {
    display: flex;
    flex-direction: column;
-}
-
-.post-input {
-   width: 100%;
-   border: 1px solid teal;
-   padding: 10px 15px;
-   margin-top: 15px;
 }
 </style>
