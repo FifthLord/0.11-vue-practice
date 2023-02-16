0c6592ae857c6cafe2970b732312812bdef82499
FifthLord <nborysenko777@gmail.com>
2023-02-16 21:53:31 +0200

    refactoring v-if to v-show

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,18 @@
 <template lang="">
-   <div>
-      <h3>Список користувачів</h3>
+   <!-- v-if - якщо умова true - відобразиться список постів -->
+   <!-- <div v-if="posts.length > 0"> -->
+<!-- замінили на v-show для гнучкості, бо при v-if - елемент повістю видаляється з ДОМ дерева-->
+   <div v-show="posts.length > 0">
+      <h3>Список постів</h3>
       <!--: - більш короткий запис v-bind:-->
       <!--передали сюди список постів як пропс, а далі вже пропс - це окремий пост-->
       <post-item :key="post.id" v-for="post in posts" :post="post" @remove="$emit('remove', post)"/>
    </div>
+   <!-- v-if - якщо умова false - відобразиться v-else (також може бути v-else-if, але v-else буде останьою)-->
+   <!-- <h2 v-else style="color: red"> -->
+   <h2 v-show="posts.length === 0" style="color: red">
+      Список постів пустий
+   </h2>
 </template>
 <script>
 import PostItem from '@/components/PostItem.vue';
